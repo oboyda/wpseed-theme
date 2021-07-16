ce1b78412c1b87b9dcc0a18c8d043b1a9aec7674
oleksiyboyda <oleksiyboyda@192.168.1.34>
2021-07-16 13:06:50 +0200

    v1.0.0.dev

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@ define('WPTB_DIR', dirname(__FILE__));
 define('WPTB_INDEX', get_stylesheet_directory_uri());
 define('WPTB_VER', wp_get_theme()->get('Version'));
 
-require WPTB_DIR . '/vendor/autoload.php';
-require WPTB_DIR . '/src/classes/load.php';
+include WPTB_DIR . '/vendor/autoload.php';
+include WPTB_DIR . '/src/classes/load.php';
 
-require WPTB_DIR . '/src/setup.php';
-require WPTB_DIR . '/src/scripts.php';
+include WPTB_DIR . '/src/setup.php';
+include WPTB_DIR . '/src/scripts.php';
 
-require WPTB_DIR . '/src/gutenberg/blocks.php';
+include WPTB_DIR . '/src/gutenberg/blocks.php';
