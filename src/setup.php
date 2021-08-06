aa7fc76781622e8e589ac744580252ee23e0c09e
oleksiyboyda <oleksiyboyda@oleksiys-mbp.home>
2021-08-06 12:42:00 +0200

    1.4.dev

--- src/setup.php
+++ src/setup.php
@@ -10,3 +10,21 @@ function wptb_load_textdomain()
 {
     load_child_theme_textdomain('wptb', WPTB_DIR . '/langs');
 }
+
+/*
+ * Setup values for oboyda/wp-seed package
+ * ----------------------------------------
+ */
+add_filter('wpseed_views_dir', 'wptb_filter_views_dir');
+
+function wptb_filter_views_dir()
+{
+    return WPTB_DIR . '/src/views';
+}
+
+add_filter('wpseed_views_namespace', 'wptb_filter_views_namespace');
+
+function wptb_filter_views_namespace()
+{
+    return '\WPTB\View';
+}
