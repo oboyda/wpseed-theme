d9bdeac1db07b8a1e31e471d04c7e74cf9ff0978
oleksiyboyda <oleksiyboyda@oleksiys-mbp.home>
2021-09-03 17:05:37 +0200

    1.6.0

--- src/views/nav-primary.php
+++ src/views/nav-primary.php
@@ -3,9 +3,9 @@
     wp_nav_menu(
         array(
             'theme_location'  => 'primary',
-            'menu_class'      => 'menu-wrapper',
-            'container_class' => 'primary-menu-container',
-            'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+            'menu_class'      => 'nav-wrapper',
+            'container_class' => 'primary-nav-container',
+            'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
             'fallback_cb'     => false
         )
     );
