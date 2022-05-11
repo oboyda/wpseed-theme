15112251ac13a6fd372c81223753c9f013ccc4b7
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2022-05-11 16:24:49 +0200

    dev

--- src/js/views-loader.js
+++ src/js/views-loader.js
@@ -1,29 +1,41 @@
+jQuery.fn.extend({
+    viewTriggerLoaded: function(triggerChildren=false)
+    {
+        this.each(function(){
+            const _view = jQuery(this);
+            const viewName = _view.data("view");
+            if(typeof viewName !== 'undefined' && viewName)
+            {
+                const triggerName = "view_loaded_" + viewName;
+                jQuery(document.body).triggerHandler(triggerName, [_view]);
+                if(triggerChildren)
+                {
+                    _view.find(".view").viewTriggerLoaded();
+                }
+            }
+        });
+    },
+    viewReplace: function(html, triggerLoadedEvent=true, triggerChildren=false)
+    {
+        this.html(html);
+        const _view = this.children();
+        this.replaceWith(_view);
+        if(triggerLoadedEvent && typeof window.triggerViewLoadedEvents !== 'undefined')
+        {
+            window.triggerViewLoadedEvents(_view, triggerChildren);
+        }
+    },
+    viewExists: function()
+    {
+        return jQuery.contains(document.body, this.get(0));
+    }
+});
+
 jQuery(function($)
 {
     /*
     * Trigger views loaded event
-    * --------------------------------------------------
+    * ----------------------------------------
     */
-
-    window.triggerViewLoadedEvents = function(view, triggerChildren=false)
-    {
-        view.each(function(){
-            const _view = $(this);
-            const viewName = _view.data("view");
-            if(viewName)
-            {
-                const triggerName = "view_loaded_" + viewName;
-                $(document.body).triggerHandler(triggerName, [_view]);
-                
-                if(triggerChildren)
-                {
-                    triggerViewLoadedEvents(_view.find(".view"));
-                }
-            }
-        });
-    };
-    
-    $(".view").each(function(){
-        window.triggerViewLoadedEvents($(this));
-    });
+    $(".view").viewTriggerLoaded();
 });
--- src/js/views.js
+++ src/js/views.js
@@ -1,34 +1,12 @@
 jQuery(function($)
 {
     /*
-    * Helpers
-    * --------------------------------------------------
-    */
-
-    const viewExists = function(view)
-    {
-        return $.contains(document.body, view.get(0));
-    };
-
-    const replaceView = function(view, html, triggerLoadedEvent=true, triggerChildren=false)
-    {
-        view.html(html);
-        const _view = view.children();
-        view.replaceWith(_view);
-        
-        if(triggerLoadedEvent && typeof window.triggerViewLoadedEvents !== 'undefined')
-        {
-            window.triggerViewLoadedEvents(_view, triggerChildren);
-        }
-    }
-
-    /*
     * .view.frist-block
     * --------------------------------------------------
     */
-    const initFirstBlock = function(e, view) 
+    initFirstBlock(e, view) 
     {
-        if(!viewExists(view)) return;
+        if(!view.viewExists()) return;
 
         //...
     };
