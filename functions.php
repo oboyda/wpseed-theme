aa80c092cd00160deaade32d3dcc5c8cb40f4f77
aleapp <admin@aleapp.com>
2022-09-12 08:07:36 +0000

    updates

--- functions.php
+++ functions.php
@@ -23,8 +23,8 @@ add_action('after_setup_theme', function(){
         require WPTBOOT_DIR . '/src/php/class-load.php';
         require WPTBOOT_DIR . '/src/php/mods.php';
         require WPTBOOT_DIR . '/src/php/scripts.php';
-        require WPTBOOT_DIR . '/src/php/acf-blocks.php';
         require WPTBOOT_DIR . '/src/php/acf-fields.php';
+        require WPTBOOT_DIR . '/src/php/acf-blocks.php';
         // require WPTBOOT_DIR . '/src/php/debug.php';
     }
     
