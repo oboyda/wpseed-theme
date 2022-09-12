fa4be22bad3b2799a12daad288c8bcbb1de1cb0b
aleapp <admin@aleapp.com>
2022-09-12 08:29:31 +0000

    updates

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ define('WPTBOOT_NAME', wp_get_theme()->get('Theme Name'));
 
 require WPTBOOT_DIR . '/vendor/autoload.php';
 require WPTBOOT_DIR . '/src/php/setup.php';
+require WPTBOOT_DIR . '/src/php/utils.php';
 
 add_action('after_setup_theme', function(){
 
@@ -19,7 +20,6 @@ add_action('after_setup_theme', function(){
 
     if($deps->check())
     {
-        require WPTBOOT_DIR . '/src/php/utils.php';
         require WPTBOOT_DIR . '/src/php/class-load.php';
         require WPTBOOT_DIR . '/src/php/mods.php';
         require WPTBOOT_DIR . '/src/php/scripts.php';
